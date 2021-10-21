1961aa8047906d845050a90b8dfd0d0da9fdb0b6
denis_levchenko <tranzi90@gmail.com>
2021-10-21 23:28:54 +0300

    add button handlers and interactivity

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
 import counter from "./store/counter";
+import {observer} from "mobx-react-lite";
 
-const Counter = () => {
+const Counter = observer(() => {
     return (
         <div>
             { `Count = ${counter.count}` }
-            <button>+</button>
-            <button>-</button>
+            <button onClick={() => counter.increment()}>+</button>
+            <button onClick={() => counter.decrement()}>-</button>
         </div>
     );
-};
+});
 
 export default Counter;
