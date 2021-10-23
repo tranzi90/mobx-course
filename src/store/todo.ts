314133869300a67885468b2153b84ab715d36d3f
denis_levchenko <tranzi90@gmail.com>
2021-10-23 19:05:46 +0300

    generate stub data

--- src/store/todo.ts
+++ src/store/todo.ts
@@ -1,7 +1,11 @@
 import {makeAutoObservable} from "mobx";
 
 class Todo {
-    todos = []
+    todos = [
+        {id: 1, title: 'Master Mobx', completed: false},
+        {id: 2, title: 'Practice React', completed: false},
+        {id: 3, title: 'Use TypeScript instead of JS', completed: false}
+    ]
 
     constructor() {
         makeAutoObservable(this)
