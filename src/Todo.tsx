056688abe306d8b5db345cc40c8bcdf2e8917a7b
denis_levchenko <tranzi90@gmail.com>
2021-10-26 19:09:53 +0300

    setup ToDo component

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,18 @@
+import {observer} from "mobx-react-lite";
+import todo from "./store/todo";
 
-const Todo = () => {
+const Todo = observer(() => {
     return (
         <div>
-
+            {todo.todos.map(todo =>
+                <div>
+                    <input type="checkbox"/>
+                    {todo.title}
+                    <button>X</button>
+                </div>
+            )}
         </div>
     );
-};
+});
 
 export default Todo;
